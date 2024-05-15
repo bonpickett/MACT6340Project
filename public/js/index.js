c9f8fcfb01e7dd209fceaaa23ad5d6864cb24ad8
Bonnie Pickett <bonniepickett@Bonnies-MacBook-Pro.local>
2024-05-14 20:23:22 -0500

    Added CSS for splash background image, added padding and section spacing, created contact form, and created contact.js.

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,3 @@
-"use strict";
-
-(() => {
-  document.querySelector("#testButton").addEventListener("click", handleClick);
-
-  function handleClick() {
-    console.log("Thank you for clicking.");
-  }
+(function () {
+  "use strict";
 })();
