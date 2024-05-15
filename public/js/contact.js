ebf8499dbf90956509fb3ddf2998ca275a6625b1
Bonnie Pickett <bonniepickett@Bonnies-MacBook-Pro.local>
2024-05-14 21:05:56 -0500

    Added event to contact.js and started the basic bootstrap grid layout for contact page.

--- public/js/contact.js
+++ public/js/contact.js
@@ -3,15 +3,15 @@
 
   document
     .querySelector("#contact-form-button")
-    .addEventListener("click", submitMail);
-
-  function submitMail() {
-    console.log("Thank you for clicking.");
-    let name = document.querySelector("#name").value;
-    let email = document.querySelector("#mail").value;
-    let message = document.querySelector("#msg").value;
-    console.log("Name: " + name);
-    console.log("Email: " + email);
-    console.log("Message: " + message);
-  }
+    .addEventListener("click", (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      console.log("Thank you for clicking.");
+      let name = document.querySelector("#name").value;
+      let email = document.querySelector("#mail").value;
+      let message = document.querySelector("#msg").value;
+      console.log("Name: " + name);
+      console.log("Email: " + email);
+      console.log("Message: " + message);
+    });
 })();
