679a456df6df62afe0e26f78e6bd70db0af1fa95
Bonnie Pickett <bonniepickett@Bonnies-MacBook-Pro.local>
2024-05-14 21:34:20 -0500

    Set up validation for contact page.

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,17 +1,32 @@
 (function () {
   "use strict";
 
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  let forms = document.querySelector("#contact-form");
+
   document
     .querySelector("#contact-form-button")
     .addEventListener("click", (event) => {
       event.preventDefault();
       event.stopPropagation();
-      console.log("Thank you for clicking.");
-      let name = document.querySelector("#name").value;
-      let email = document.querySelector("#mail").value;
-      let message = document.querySelector("#msg").value;
-      console.log("Name: " + name);
-      console.log("Email: " + email);
-      console.log("Message: " + message);
+      let formValid = true;
+      if (!form.checkValidity()) {
+        formValid = false;
+      }
+      form.classList.add("was-validated");
+      if (formValid) {
+        sendTheEmail();
+      }
     });
+  function sendTheEmail() {
+    console.log("Thank you for clicking.");
+    let firstName = document.querySelector("#first-name").value;
+    let lastName = document.querySelector("#last-name").value;
+    let email = document.querySelector("#mail").value;
+    let message = document.querySelector("#msg").value;
+    console.log("First Name: " + firstName);
+    console.log("Last Name: " + lastName);
+    console.log("Email: " + email);
+    console.log("Message: " + message);
+  }
 })();
