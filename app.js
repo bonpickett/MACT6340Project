d317caec9a2419c25519a53d710a4b3b9be6fc46
Bonnie Pickett <bonniepickett@Bonnies-MacBook-Pro.local>
2024-05-27 20:24:33 -0500

    Set up app.js to pass data through for individual project pages. Added header and footer ejs pages and update all other pages to reflect the header and footer.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
+let data = ["Project 1", "Project 2", "Project 3"];
 
 const app = express();
 const port = 3000;
@@ -14,7 +15,15 @@ app.get("/", (req, res) => {
 });
 
 app.get("/projects", (req, res) => {
-  res.render("projects.ejs");
+  res.render("projects.ejs", { projectArray: data });
+});
+
+app.get("/project/:id", (req, res) => {
+  let id = req.params.id;
+  if (id > data.length) {
+    throw new Error("No project with that ID");
+  }
+  res.render("project.ejs", { projectArray: data, which: id });
 });
 
 app.get("/contact", (req, res) => {
@@ -31,6 +40,11 @@ app.post("/mail", async (req, res, err) => {
       console.log(err);
       res.send({ result: "Message failed to send" });
     });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.render("error.ejs");
 });
 
 app.listen(port, () => {
