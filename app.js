ab93ddce1fcbdc2a2b82f62f28a78639a53ddca5
Bonnie Pickett <bonniepickett@Bonnies-MacBook-Pro.local>
2024-05-27 19:13:05 -0500

    Added Views folder and converted html files to ejs. Updated nav links to render to correct points for ejs.

--- app.js
+++ app.js
@@ -5,18 +5,31 @@ dotenv.config();
 
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
 
 app.post("/mail", async (req, res, err) => {
   await utils
     .sendMessage(req.body.sub, req.body.txt)
     .then(() => {
-      res.send({ result: "success" });
+      res.send({ result: "Message Sent!" });
     })
     .catch((err) => {
       console.log(err);
-      res.send({ result: "failure" });
+      res.send({ result: "Message failed to send" });
     });
 });
 
