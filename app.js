1dd4dd0010f3fd2ea024cfe25115fca9f3ae6adb
Bonnie Pickett <bonniepickett@Bonnies-MacBook-Pro.local>
2024-05-29 20:07:36 -0500

    Set up SQL database and cleaned up ejs files.

--- app.js
+++ app.js
@@ -11,42 +11,42 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+ res.render("index.ejs");
 });
 
 app.get("/projects", (req, res) => {
-  res.render("projects.ejs", { projectArray: data });
+ res.render("projects.ejs", { projectArray: data });
 });
 
 app.get("/project/:id", (req, res) => {
-  let id = req.params.id;
-  if (id > data.length) {
-    throw new Error("No project with that ID");
-  }
-  res.render("project.ejs", { projectArray: data, which: id });
+ let id = req.params.id;
+ if (id > data.length) {
+  throw new Error("No project with that ID");
+ }
+ res.render("project.ejs", { projectArray: data, which: id });
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
+ res.render("contact.ejs");
 });
 
 app.post("/mail", async (req, res, err) => {
-  await utils
-    .sendMessage(req.body.sub, req.body.txt)
-    .then(() => {
-      res.send({ result: "Message Sent!" });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send({ result: "Message failed to send" });
-    });
+ await utils
+  .sendMessage(req.body.sub, req.body.txt)
+  .then(() => {
+   res.send({ result: "Message Sent!" });
+  })
+  .catch((err) => {
+   console.log(err);
+   res.send({ result: "Message failed to send" });
+  });
 });
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  res.render("error.ejs");
+ console.log(err);
+ res.render("error.ejs");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+ console.log(`Example app listening on port ${port}`);
 });
